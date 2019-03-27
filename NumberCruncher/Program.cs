72211d99e760d8d4c241f7136b78c5da1161f118
CailieAC <cailiecarlile@gmail.com>
2019-03-26 22:55:54 -0500

    commit1 add comments for problem statement

--- NumberCruncher/Program.cs
+++ NumberCruncher/Program.cs
@@ -6,7 +6,14 @@ namespace NumberCruncher
     {
         public static void Main()
         {
+            //Complete the Number Cruncher program that has a static method to 
+            //find the sum of all the even numbers in a list. 
+            //This method should be in a class called NumberCruncher in its own class library called NumberUtility. 
+            //Within your console project, create a list with at least 10 integers and call your method on the list.
+
+
             Console.ReadLine();
         }
     }
+    
 }
---
+++ NumberUtility/NumberCruncher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberUtility
+{
+    //Complete the Number Cruncher program that has a static method to 
+    //find the sum of all the even numbers in a list. 
+    //This method should be in a class called NumberCruncher in its own class library called NumberUtility. 
+    //Within your console project, create a list with at least 10 integers and call your method on the list.
+
+    class NumberCruncher
+    {
+
+
+    }
+}
